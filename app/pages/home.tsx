bd2d6de86b8bd8a02f9bb31221ae77280b8809f2
Devin B <devin.c.blair@gmail.com>
2025-02-16 15:32:40 -0800

    Added fonts and colors to Tailwind theme. Created base to account for minimum body styling. Added placeholder for tabs.

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 function Home() {
   return (
     <div className="size-full flex flex-col">
-      <span className="">hey there!</span>
+      <span className="">tabs go here</span>
       <Outlet />
     </div>
   );
