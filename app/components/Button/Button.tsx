258d1a6f6ff9400351bdc6e5123b8a7681385c11
Devin B <devin.c.blair@gmail.com>
2025-02-17 15:55:24 -0800

    Build Button component with variants. Add Button to Locations and Variables.

---
+++ app/components/Button/Button.tsx
@@ -0,0 +1,50 @@
+import { filterClasses } from "@/utils/filterClasses";
+import { ComponentPropsWithoutRef, memo, type ReactNode } from "react";
+
+const variants = {
+  primary: [
+    "bg-orange-150 text-orange-600 rounded-sm px-2 py-1.5 drop-shadow-sm",
+    "disabled:text-neutral-700 disabled:bg-neutral-300",
+    "hover:bg-orange-200 drop-shadow-md",
+  ],
+  link: [
+    "underline",
+    "focus-visible:outline-neutral-600 focus-visible:outline-offset-4",
+    "disabled:text-neutral-350",
+    "hover:text-neutral-500",
+  ],
+};
+
+type Variant = keyof typeof variants;
+
+interface ButtonProps
+  extends Pick<
+    ComponentPropsWithoutRef<"button">,
+    "children" | "type" | "onClick"
+  > {
+  children: ReactNode;
+  classNames?: string;
+  variant?: Variant;
+}
+
+const BaseButton = ({
+  classNames = "",
+  children,
+  variant = "primary",
+  ...rest
+}: ButtonProps) => {
+  return (
+    <button
+      className={filterClasses(
+        "transition duration-300 ease-in-out",
+        variants[variant],
+        classNames
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export const Button = memo(BaseButton);
