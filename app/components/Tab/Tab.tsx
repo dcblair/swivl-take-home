686bb71d18ac24ecf70394509830c1cc95c76edf
Devin B <devin.c.blair@gmail.com>
2025-02-17 01:08:53 -0800

    Update prop names. Add disabled styles, fix standard and active styles.

--- app/components/Tab/Tab.tsx
+++ app/components/Tab/Tab.tsx
@@ -1,20 +1,20 @@
 import { filterClasses } from "@/utils/filterClasses";
-import { type CSSProperties, memo } from "react";
-import { NavLink } from "react-router";
+import { type CSSProperties, memo, type ReactNode } from "react";
+import { NavLink, NavLinkProps } from "react-router";
 
-interface TabProps {
+interface TabProps extends NavLinkProps {
   active?: boolean;
   classNames?: string;
   styles?: Partial<CSSProperties>;
-  name: string;
+  children: ReactNode;
   to: string;
 }
 
 const BaseTab = ({
-  active = true,
+  active = false,
   classNames = "",
-  name,
-  styles,
+  children,
+  styles = {},
   to,
   ...rest
 }: TabProps) => {
@@ -23,15 +23,16 @@ const BaseTab = ({
   return (
     <NavLink
       className={filterClasses(
-        "",
-        active && "bg-orange-150 text-orange-500",
+        "text-black tracking-wide text-xl px-4 py-2.5 rounded-lg w-fit",
+        active && "bg-orange-150 text-orange-500 font-semibold",
+        "aria-disabled:text-neutral-400 aria-disabled:bg-neutral-200 pointer-events-none",
         classNames
       )}
       style={styles}
       to={to}
       {...rest}
     >
-      {name}
+      {children}
     </NavLink>
   );
 };
