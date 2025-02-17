c0043220f67c0fa1de6db5c6bee568ff68808f85
Devin B <devin.c.blair@gmail.com>
2025-02-17 11:26:07 -0800

    Build out Tabs component and render in home.

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -1,10 +1,22 @@
+import { Tabs } from "@/components/Tabs/Tabs";
 import { Outlet } from "react-router";
 
 function Home() {
+  const tabs = [
+    { name: "Details", href: "/" },
+    { name: "Cortex", href: "/cortex" },
+    { name: "Escalations", href: "/escalations" },
+    { name: "Sequences", href: "/sequences" },
+    { name: "Locations and Variables", href: "/locations-variables" },
+    { name: "spot.is", href: "/spot-is" },
+  ];
+
   return (
     <div className="size-full flex flex-col">
-      <span className="">tabs go here</span>
-      <Outlet />
+      <Tabs aria-label="navigation" tabs={tabs} />
+      <div className="py-8 px-6">
+        <Outlet />
+      </div>
     </div>
   );
 }
