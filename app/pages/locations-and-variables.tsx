4d877e3158b17880e9dd84105cba280b36b5a62c
Devin B <devin.c.blair@gmail.com>
2025-02-17 16:07:34 -0800

    Add barrel export for components.

--- app/pages/locations-and-variables.tsx
+++ app/pages/locations-and-variables.tsx
@@ -1,5 +1,4 @@
-import { Button } from "@/components/Button/Button";
-import { Card } from "@/components/Card/Card";
+import { Button, Card } from "@/components";
 
 function LocationsAndVariables() {
   /**
