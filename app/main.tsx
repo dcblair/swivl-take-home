2a657c653e7ae5da1ad1406a00eb4b30816c9c50
Devin B <devin.c.blair@gmail.com>
2025-02-17 17:43:56 -0800

    Set up .env.example. Update README. Install zustand and Tanstack/React Query. Add provider and build basic query for variables and locations.

--- app/main.tsx
+++ app/main.tsx
@@ -1,27 +1,32 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./tailwind.css";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "@/pages/home";
 import LocationsAndVariables from "@/pages/locations-and-variables";
+import "./tailwind.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Home />}>
-          {/* Treating Details tab as index */}
-          <Route index element={<span>Details</span>} />
-          <Route path="/cortex" element={<span>Cortex</span>} />
-          <Route path="/escalations" element={<span>Escalations</span>} />
-          <Route path="/sequences" element={<span>Sequences</span>} />
-          <Route
-            path="/locations-and-variables"
-            element={<LocationsAndVariables />}
-          />
-          <Route path="/spot-is" element={<span>spot.is</span>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home />}>
+            {/* Treating Details tab as index */}
+            <Route index element={<span>Details</span>} />
+            <Route path="/cortex" element={<span>Cortex</span>} />
+            <Route path="/escalations" element={<span>Escalations</span>} />
+            <Route path="/sequences" element={<span>Sequences</span>} />
+            <Route
+              path="/locations-and-variables"
+              element={<LocationsAndVariables />}
+            />
+            <Route path="/spot-is" element={<span>spot.is</span>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
--- app/pages/locations-and-variables.tsx
+++ app/pages/locations-and-variables.tsx
@@ -1,19 +1,64 @@
 import { Button, Card } from "@/components";
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
 
 function LocationsAndVariables() {
-  /**
-   *  consider logic for showing variables
-   *  isshowing per location?
-   *  const [isShowing, setIsShowing] = useState({
-   *  location1: false,
-   *  location2: true,
-   *  location3: true
-   * })
-   *  additionally, do i want to persist this data so that variables will remain showing when users leave and return?
-   *  */
+  const navigate = useNavigate();
+
+  const {
+    data: variables,
+    error: variablesError,
+    isPending: areVariablesLoading,
+  } = useQuery({
+    queryKey: ["variablesData"],
+    queryFn: async () => {
+      const variableRes = await fetch(
+        `${import.meta.env.VITE_SWIVL_BASE_URL}/variables`
+      );
+      const variables = await variableRes.json();
+      return variables;
+    },
+  });
+
+  const {
+    data: locations,
+    error: locationsError,
+    isPending: areLocationsLoading,
+  } = useQuery({
+    queryKey: ["locationsData"],
+    queryFn: async () => {
+      const locationsRes = await fetch(
+        `${import.meta.env.VITE_SWIVL_BASE_URL}/locations`
+      );
+      const locationss = await locationsRes.json();
+      return locationss;
+    },
+  });
+  // 3 locations under 1 org
+  //
+  // keys: name, address, phone number
+  // grab value of key to display
+
+  // todo: flesh out error state
+  if (locationsError || variablesError) {
+    return (
+      <div>
+        <p>
+          Something went wrong.{" "}
+          <Button variant="link" onClick={() => navigate(0)}>
+            Click here to refresh.
+          </Button>
+        </p>
+      </div>
+    );
+  }
+
+  console.log(variables, "variables", locations, "locations");
 
   return (
     <div className="flex flex-col space-y-6">
+      {/* todo: flesh out loading state */}
+      {areLocationsLoading || (areVariablesLoading && "loading")}
       <Card>
         <div>
           <h2 className="font-poppins">Sickvile</h2>
